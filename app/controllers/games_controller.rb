c9662dec5eddd48f90d9829916bc6014cd50beea
nsemmler <ndsunc@gmail.com>
2016-10-20 12:35:00 -0400

    Working login, logout, sign up pages with username displayed when logged in, and username used as log in identifier.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,3 @@
 class GamesController < ApplicationController
+  before_action :require_user, only: [:index, :show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161020152940) do
 
   # These are extensions that must be enabled in order to support this database
@@ -21,6 +20,7 @@ ActiveRecord::Schema.define(version: 20161020152940) do
     t.string   "card_number"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
