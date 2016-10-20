1218bf61954b2e5d7cab18057af4e17248a4132a
Daniel Griffiths <logiconictvtube@gmail.com>
2016-10-20 12:36:08 -0400

    score table refrenced to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20161020152940) do
+ActiveRecord::Schema.define(version: 20161020162833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +20,16 @@ ActiveRecord::Schema.define(version: 20161020152940) do
     t.string   "card_number"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "scores", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "completeness"
+    t.integer  "turns_taken"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["user_id"], name: "index_scores_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -30,4 +39,5 @@ ActiveRecord::Schema.define(version: 20161020152940) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "scores", "users"
 end
