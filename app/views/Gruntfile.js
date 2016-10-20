e3eafaf7cadcdb919f1938433722f3e26dceb109
Marian Powell <marianpowell@gmail.com>
2016-10-20 14:27:56 -0400

    updated file structures

---
+++ app/views/Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'lib/styles/main.min.css': 'src/styles/main.scss'
+                }
+            }
+        },
+
+        haml: {
+            dist: {
+                files: {
+                    'layouts/index.html':'layouts/index.haml',
+                    'layouts/leaderBoard.html':'layouts/leaderBoard.haml'
+                }
+            }
+        },
+
+        babel: {
+            options: {
+                sourceMap: true,
+                presets: ['es2015']
+            },
+            dist: {
+                files: {
+                    'lib/js/app.js': 'src/js/app.js'
+                }
+            }
+        },
+
+        watch: {
+            css: {
+                files: ['src/styles/**/*'],
+                tasks: ['sass']
+            },
+
+            html: {
+              files: ['layouts/*.haml'],
+              tasks: ['haml']
+            },
+            javascript: {
+              files: [
+                'src/js/**/*'
+              ],
+              tasks: ['babel']
+},
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-haml2html');
+    grunt.loadNpmTasks('grunt-babel');
+    grunt.registerTask('default', [
+        'sass',
+        'watch',
+        'haml',
+        'babel'
+    ]);
+};
---
+++ app/views/src/js/app.js
@@ -0,0 +1,2 @@
+const a = 0;
+let b = 1;
