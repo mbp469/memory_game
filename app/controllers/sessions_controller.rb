8331bb92cb83e54c409b6e682147e13a672eddd4
nsemmler <ndsunc@gmail.com>
2016-10-20 16:49:35 -0400

    created route after login to games page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     @user = User.find_by_username(params[:session][:username])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      redirect_to '/'
+      redirect_to '/games'
     else
       redirect_to '/login'
     end
