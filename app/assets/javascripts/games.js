8517c40bb5f76dfa252bd52bb772407356fb3534
nsemmler <ndsunc@gmail.com>
2016-10-22 15:20:25 -0400

    Methods to shuffle array of cards for each difficulty

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -106,14 +106,15 @@ var cards = [
   },
 ];
 
-var memoryCards = [] //run selectDifficulty before caling memoryCards
+var memoryCards = []
+var shuffledMemoryCards = []
 
 function selectDifficulty(difficulty) {
-  if(difficulty=='easy') {
+  if(difficulty=='Easy') {
     gameCards = getCardsByDifficulty(cards, 4)
-  } else if(difficulty=='medium') {
+  } else if(difficulty=='Medium') {
     gameCards = getCardsByDifficulty(cards, 8)
-  } else if(difficulty=='hard') {
+  } else if(difficulty=='Hard') {
     gameCards = cards
   }
   memoryCards = gameCards.concat(gameCards)
@@ -123,6 +124,33 @@ function getCardsByDifficulty(cardsArray, neededNumberCards) {
   var cardsByDifficulty = [];
   for (var i = 0; i < neededNumberCards; i++) {
     cardsByDifficulty.push(cardsArray[Math.floor(Math.random()*cardsArray.length)]);
-  }
+  } //try to make conditional for above line - not repeat cards chosen
   return cardsByDifficulty;
 }
+
+function shuffleMemoryCards(cardsArray) {
+  shuffledMemoryCards = this.shuffle(cardsArray);
+}
+
+// Fisher-Yates Algorithm
+function shuffle(array) {
+  var counter = array.length, temp, index;
+
+  while (counter > 0) {
+      index = Math.floor(Math.random() * counter);
+
+      counter--;
+
+      temp = array[counter];
+      array[counter] = array[index];
+      array[index] = temp;
+    }
+    return array;
+}
+
+
+
+//run selectDifficulty('thedifficulty')
+//now all pairs exist for game, but in identical order
+//run shuffleMemoryCards(memoryCards)
+//now shuffledMemoryCards is in the order which to use in game
