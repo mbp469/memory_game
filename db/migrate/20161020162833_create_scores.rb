14423ca53c8cfcb906141dc8ce82c47bb2622d41
Daniel Griffiths <logiconictvtube@gmail.com>
2016-10-22 13:34:42 -0400

    revised score table

--- db/migrate/20161020162833_create_scores.rb
+++ db/migrate/20161020162833_create_scores.rb
@@ -2,11 +2,8 @@ class CreateScores < ActiveRecord::Migration[5.0]
   def change
     create_table :scores do |t|
       t.integer :user_id
-      t.integer :completeness
       t.integer :turns_taken
       t.references :user, foreign_key: true
-
-      t.timestamps
     end
   end
 end
