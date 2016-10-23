982f02e27386effe4e6090cdbafd981a86cb66b7
Marliana Lara <marliana.lara@gmail.com>
2016-10-23 12:28:59 -0400

    remove eventhandler on matched cards

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -18,6 +18,7 @@ $(document).on('turbolinks:load', function() {
       scrollTop: $(".board-wrap").offset().top
     }, 1000);
 
+    $('.card').on('click', handleCardClick);
   });
 
 
@@ -37,7 +38,7 @@ $(document).on('turbolinks:load', function() {
     $('#board-container').append(card);
   }
 
-  $(document).on('click', '.card', function() {
+  function handleCardClick() {
     var state = $(this).attr('data-card-state');
     $(this).find('.flipper').toggleClass('flip');
 
@@ -46,49 +47,70 @@ $(document).on('turbolinks:load', function() {
     } else if (state === 'active') {
       $(this).attr('data-card-state', 'inactive');
     }
-  });
+
+    var activeCards = $('[data-card-state=active]');
+    setTimeout(checkMatch, 500, activeCards);
+  };
+
+
+  function checkMatch(activeCards) {
+    if (activeCards.length === 2) {
+      if(activeCards[0].dataset.cardId === activeCards[1].dataset.cardId) {
+        // activeCards[0].removeEventListener('click', handleCardClick);
+        activeCards.off();
+        // activeCards[1].removeEventListener('click', handleCardClick);
+        activeCards[0].dataset.cardState = 'matched';
+        activeCards[1].dataset.cardState = 'matched';
+        console.log('match');
+      } else {
+        activeCards[0].dataset.cardState = 'inactive';
+        $(activeCards[0]).find('.flipper').toggleClass('flip');
+        activeCards[1].dataset.cardState = 'inactive';
+        $(activeCards[1]).find('.flipper').toggleClass('flip');
+
+      }
+    }
+  }
+  function selectDifficulty(difficulty) {
+    if(difficulty=='Easy') {
+      gameCards = getCardsByDifficulty(4);
+    } else if(difficulty=='Medium') {
+      gameCards = getCardsByDifficulty(8);
+    } else if(difficulty=='Hard') {
+      gameCards = getCardsByDifficulty(12);
+    }
+    return gameCards.concat(gameCards);
+  }
+
+  function getCardsByDifficulty(neededNumberPairs) {
+    var cardsByDifficulty = [];
+
+    for (var i = 1; i <= neededNumberPairs; i++) {
+      cardsByDifficulty.push(i);
+    }
+    return cardsByDifficulty;
+  }
+
+  function shuffleMemoryCards(cardsArray) {
+    return shuffle(cardsArray);
+  }
+
+  // Fisher-Yates Algorithm
+  function shuffle(array) {
+    var counter = array.length, temp, index;
+
+    while (counter > 0) {
+      index = Math.floor(Math.random() * counter);
+
+      counter--;
+
+      temp = array[counter];
+      array[counter] = array[index];
+      array[index] = temp;
+    }
+    return array;
+  }
 });
-
-function selectDifficulty(difficulty) {
-  if(difficulty=='Easy') {
-    gameCards = getCardsByDifficulty(4);
-  } else if(difficulty=='Medium') {
-    gameCards = getCardsByDifficulty(8);
-  } else if(difficulty=='Hard') {
-    gameCards = getCardsByDifficulty(12);
-  }
-  return gameCards.concat(gameCards);
-}
-
-function getCardsByDifficulty(neededNumberPairs) {
-  var cardsByDifficulty = [];
-
-  for (var i = 1; i <= neededNumberPairs; i++) {
-    cardsByDifficulty.push(i);
-  }
-  return cardsByDifficulty;
-}
-
-function shuffleMemoryCards(cardsArray) {
-  return shuffle(cardsArray);
-}
-
-// Fisher-Yates Algorithm
-function shuffle(array) {
-  var counter = array.length, temp, index;
-
-  while (counter > 0) {
-    index = Math.floor(Math.random() * counter);
-
-    counter--;
-
-    temp = array[counter];
-    array[counter] = array[index];
-    array[index] = temp;
-  }
-  return array;
-}
-
 
 //run selectDifficulty('thedifficulty')
 //now all pairs exist for game, but in identical order
