0a11f61768f5f64ba382f23393a690450a020193
nsemmler <ndsunc@gmail.com>
2016-10-23 16:37:46 -0400

    Removed comments from public/assets/application-a921...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,12 +19,9 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-<<<<<<< HEAD
-=======
 
   def update
   end
->>>>>>> 3d828eaf6fa48b6c58b8e9a7b616ece9cec2238e
 
   private
   def user_params
