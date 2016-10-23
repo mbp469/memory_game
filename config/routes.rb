40e962b9e2167ac3b645c4fd3f852ceb90f451bf
nsemmler <ndsunc@gmail.com>
2016-10-23 15:06:50 -0400

    route to post to table when user wins

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
 
   delete 'login' => 'sessions#destroy'
+
+  # post 'game' => 'scores#win'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :games
