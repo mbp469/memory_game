9771219dec38054b5a66be8309bf1b646d200f3e
nsemmler <ndsunc@gmail.com>
2016-10-22 23:20:57 -0400

    removes duplicate pairs from selected cards

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,41 +1,90 @@
 $(document).on('turbolinks:load', function() {
 
-  $(document).one('click', '.difficulty', function() {
-    var numCards = 1;
-
-    var level = $(this).val();
-
-    selectDifficulty(level);
-    shuffleMemoryCards(memoryCards);
-
-    shuffledMemoryCards.forEach(function(card) {
-      var cardId = card.id;
-      createCard(cardId);
-    });
-  });
-
-
-  function createCard(id) {
-    var front = document.createElement('div'),
-    back = document.createElement('div'),
-    flipper = document.createElement('div'),
-    card = document.createElement('div');
-
-    front.className = 'front';
-    back.className = 'back';
-    flipper.className = 'flipper';
-    card.className = 'card';
-
-    $(flipper).append(front, back);
-    $(card).attr('id', id).append(flipper);
-    $('#board-container').append(card);
-  }
+  $(document).one('click', '.difficulty', createCards);
 
   $(document).on('click', '.card', function() {
     $(this).find('.flipper').toggleClass('flip');
   });
 });
 
+function createCards() {
+  var level = $(this).val();
+
+  selectDifficulty(level);
+  shuffleMemoryCards(memoryCards);
+
+  shuffledMemoryCards.forEach(function(card) {
+    var cardId = card.id;
+    assignCardIDs(cardId);
+  });
+}
+
+function assignCardIDs(id) {
+  var front = document.createElement('div'),
+  back = document.createElement('div'),
+  flipper = document.createElement('div'),
+  card = document.createElement('div');
+
+  front.className = 'front';
+  back.className = 'back';
+  flipper.className = 'flipper';
+  card.className = 'card';
+
+  $(flipper).append(front, back);
+  $(card).attr('id', id).append(flipper);
+  $('#board-container').append(card);
+}
+
+var memoryCards = [];
+var shuffledMemoryCards = [];
+
+function selectDifficulty(difficulty) {
+  if (difficulty=='Easy') {
+    gameCards = getCardsByDifficulty(cards, 4);
+  } else if (difficulty=='Medium') {
+    gameCards = getCardsByDifficulty(cards, 8);
+  } else if (difficulty=='Hard') {
+    gameCards = cards;
+  }
+
+  memoryCards = gameCards.concat(gameCards)
+}
+
+function getCardsByDifficulty(cardsArray, neededNumberOfPairs) {
+  var cardsNoDuplicates = [];
+
+  while (cardsNoDuplicates.length < neededNumberOfPairs) {
+    cardsNoDuplicates.push(cardsArray[Math.floor(Math.random()*cardsArray.length)]);
+    var uniqueMemoryCards = cardsNoDuplicates.filter(eliminateDuplicates)
+    cardsNoDuplicates = uniqueMemoryCards
+  }
+  return uniqueMemoryCards;
+}
+
+function shuffleMemoryCards(cardsArray) {
+  shuffledMemoryCards = this.shuffle(cardsArray);
+}
+
+function eliminateDuplicates(element, index, self) {
+  return index == self.indexOf(element);
+}
+
+// Fisher-Yates Algorithm
+function shuffle(array) {
+  var counter = array.length, temp, index;
+
+  while (counter > 0) {
+    index = Math.floor(Math.random() * counter);
+
+    counter--;
+
+    temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+
+  return array;
+}
 
 var cards = [
   {
@@ -87,53 +136,6 @@ var cards = [
     id: 12
   },
 ];
-
-var memoryCards = [];
-var shuffledMemoryCards = [];
-
-function selectDifficulty(difficulty) {
-  if(difficulty=='Easy') {
-    gameCards = getCardsByDifficulty(cards, 4);
-  } else if(difficulty=='Medium') {
-    gameCards = getCardsByDifficulty(cards, 8);
-  } else if(difficulty=='Hard') {
-    gameCards = cards;
-  }
-
-  memoryCards = gameCards.concat(gameCards)
-}
-
-function getCardsByDifficulty(cardsArray, neededNumberPairs) {
-  var cardsByDifficulty = [];
-
-  for (var i = 0; i < neededNumberPairs; i++) {
-    cardsByDifficulty.push(cardsArray[Math.floor(Math.random()*cardsArray.length)]);
-  } //try to make conditional for above line - not repeat cards chosen
-  return cardsByDifficulty;
-}
-
-function shuffleMemoryCards(cardsArray) {
-  console.log('we in');
-  shuffledMemoryCards = this.shuffle(cardsArray);
-}
-
-// Fisher-Yates Algorithm
-function shuffle(array) {
-  var counter = array.length, temp, index;
-
-  while (counter > 0) {
-    index = Math.floor(Math.random() * counter);
-
-    counter--;
-
-    temp = array[counter];
-    array[counter] = array[index];
-    array[index] = temp;
-  }
-
-  return array;
-}
-
 
 //run selectDifficulty('thedifficulty')
 //now all pairs exist for game, but in identical order
