44516213f0d882954bc95309d273cd005a991295
nsemmler <ndsunc@gmail.com>
2016-10-23 14:43:00 -0400

    changed delete route to login url through sessions destroy

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   post 'login' => 'sessions#create'
 
-  delete 'logout' => 'sessions#destroy'
+  delete 'login' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :games
